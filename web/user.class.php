a8d523169871d1732d303e99a61a31f1b745b175
Corey Hunter <Oishikatta+github.com@regex.be>
2016-11-05 14:05:38 -0700

    update

--- web/user.class.php
+++ web/user.class.php
@@ -30,3 +30,53 @@ class User {
 		);
 	}
 }
+
+class Node {
+	public var $id;
+	public var $name;
+	public var $template;
+	public var $children = array();
+
+	public function Node($properties) {
+		foreach($properties as $key => $value) {
+			$this->key = $value;
+		}
+	}
+
+	public function setId($id) {
+		$this->id = $id;
+		return this;
+	}
+
+	public function setTemplate($template) {
+		$this->template = $template;
+		return this;
+	}
+
+	public function addChild($childNode) {
+		array_push($this->children, $childNode);
+		return this;
+	}
+}
+
+function buildTree() {
+
+	// First node has two children -- Get Job or Go to College
+	var $nodeProperties = array(
+		"id" => 0,
+		"name" => "Job or College",
+		"template" => "job_or_college.twig"
+	);
+
+	var $startNode = new Node($nodeProperties);
+
+	$nodeProperties = array(
+		"id"		=> 1,
+		"name"		=> "Get a Job",
+		"template"	=> "choose_job.twig"
+	);
+
+	$startNode->addChild($nodeProperties);
+
+}
+
