f67bc4ecb5def8ed89f08dfa648081ac037bf265
1 <1>
2016-11-05 19:40:11 -0700

    w

--- web/nodes_job.php
+++ web/nodes_job.php
@@ -1,4 +1,7 @@
 <?php
+
+#Work options
+
 $nodeProperties = array(
 		"id"		=> 1000,
 		"name"		=> "Military",
@@ -8,6 +11,17 @@ $nodeProperties = array(
 	array_push($nodes, new Node($nodeProperties));
 	$militaryNode = $nodes[count($nodes)-1];
 	$jobNode->addChild($nodes[count($nodes)-1]);
+	
+	
+	        $nodeProperties = array(
+		        "id"		=> 2000,
+		        "name"		=> "Start a checking account",
+		        "template"	=> "choose_Checking.twig"
+	        );
+
+	        array_push($nodes, new Node($nodeProperties));
+	        $checkingNode = $nodes[count($nodes)-1];
+	        $militaryNode->addChild($nodes[count($nodes)-1]);
 
 $nodeProperties = array(
 		"id"		=> 1001,
@@ -18,6 +32,16 @@ $nodeProperties = array(
 	array_push($nodes, new Node($nodeProperties));
 	$constructionNode = $nodes[count($nodes)-1];
 	$jobNode->addChild($nodes[count($nodes)-1]);
+	
+	         $nodeProperties = array(
+		        "id"		=> 2001,
+		        "name"		=> "Start a checking account",
+		        "template"	=> "choose_Checking.twig"
+	        );
+
+	        array_push($nodes, new Node($nodeProperties));
+	        $checking1Node = $nodes[count($nodes)-1];
+	        $constructionNode->addChild($nodes[count($nodes)-1]);
 
 $nodeProperties = array(
 		"id"		=> 1002,
@@ -38,4 +62,48 @@ $nodeProperties = array(
 	array_push($nodes, new Node($nodeProperties));
 	$retailNode = $nodes[count($nodes)-1];
 	$jobNode->addChild($nodes[count($nodes)-1]);
+	
+	
+	
+	
+# College
 
+$nodeProperties = array(
+		"id"		=> 3000,
+		"name"		=> "Trade School",
+		"template"	=> "choose_Trade.twig"
+	);
+
+	array_push($nodes, new Node($nodeProperties));
+	$tradeNode = $nodes[count($nodes)-1];
+	$collegeNode->addChild($nodes[count($nodes)-1]);
+	
+$nodeProperties = array(
+		"id"		=> 3001,
+		"name"		=> "Community College",
+		"template"	=> "choose_Community.twig"
+	);
+
+	array_push($nodes, new Node($nodeProperties));
+	$communityNode = $nodes[count($nodes)-1];
+	$collegeNode->addChild($nodes[count($nodes)-1]);
+	
+$nodeProperties = array(
+		"id"		=> 3002,
+		"name"		=> "State School",
+		"template"	=> "choose_State.twig"
+	);
+
+	array_push($nodes, new Node($nodeProperties));
+	$stateNode = $nodes[count($nodes)-1];
+	$collegeNode->addChild($nodes[count($nodes)-1]);
+	
+$nodeProperties = array(
+		"id"		=> 3003,
+		"name"		=> "University",
+		"template"	=> "choose_University.twig"
+	);
+
+	array_push($nodes, new Node($nodeProperties));
+	$universityNode = $nodes[count($nodes)-1];
+	$collegeNode->addChild($nodes[count($nodes)-1]);
